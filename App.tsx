440e60cb3ee470e828f7b8fe49a59b2a40339ff3
Tiago Martins <tiagomartinscc@hotmail.com>
2025-03-12 12:32:08 -0300

    install GluestackUI

--- App.tsx
+++ App.tsx
@@ -5,21 +5,25 @@ import {
   Roboto_700Bold 
 } from '@expo-google-fonts/roboto'
 
+import { GluestackUIProvider } from '@gluestack-ui/themed'
+
 export default function App() {
   const [ fontsLoaded ] = useFonts({Roboto_400Regular, Roboto_700Bold})
   return (
-    <View style={{
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
-      backgroundColor: '#202024'
-    }}>
-      <StatusBar 
-        barStyle='light-content' 
-        backgroundColor='transparent'
-        translucent
-      />
-      {fontsLoaded ? <Text>Home</Text> : <View />}
-    </View>
+    <GluestackUIProvider>
+      <View style={{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#202024'
+      }}>
+        <StatusBar 
+          barStyle='light-content' 
+          backgroundColor='transparent'
+          translucent
+        />
+        {fontsLoaded ? <Text>Home</Text> : <View />}
+      </View>
+    </GluestackUIProvider>
   );
 }
