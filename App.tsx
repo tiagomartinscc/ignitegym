e92554fccffadfc480af53e1b776beaa65a9c2e2
Tiago Martins <tiagomartinscc@hotmail.com>
2025-03-13 11:02:36 -0300

    sign up screen

--- App.tsx
+++ App.tsx
@@ -8,7 +8,7 @@ import {
 import { GluestackUIProvider, Center } from '@gluestack-ui/themed'
 import { config } from './config/gluestack-ui.config'
 import { Loading } from '@components/Loading';
-import { SignIn } from '@screens/SignIn';
+import { SignUp } from '@screens/SignUp';
 
 export default function App() {
   const [ fontsLoaded ] = useFonts({Roboto_400Regular, Roboto_700Bold})
@@ -20,7 +20,7 @@ export default function App() {
         translucent
       />
       {fontsLoaded ? (
-        <SignIn />
+        <SignUp />
       ) : (
         <Loading />
       )}
---
+++ src/screens/SignUp.tsx
@@ -0,0 +1,59 @@
+import { VStack, Image, Center, Text, Heading, ScrollView } from "@gluestack-ui/themed";
+import BackgroundImg from '@assets/background.png'
+import Logo from '@assets/logo.svg'
+import { Input } from "@components/Input";
+import { Button } from "@components/Button";
+
+export function SignUp() {
+  return (
+    <ScrollView 
+      contentContainerStyle={{ flexGrow: 1}}
+      showsVerticalScrollIndicator={false}
+    >
+      <VStack flex={1} bg="$gray700">
+        <Image
+          w="$full"
+          h={624}
+          source={BackgroundImg}
+          defaultSource={BackgroundImg}
+          alt="Pessoas treinando"
+          position="absolute"
+        />
+        <Center my="$24">
+          <Logo />
+
+          <Text color="$gray100" fontSize="$sm">
+            Treine a sua mente e seu corpo
+          </Text>
+        </Center>
+
+        <VStack flex={1} px="$10" pb="$16">
+          <Center gap="$2" flex={1}>
+            <Heading color="$gray100">
+              Crie sua conta
+            </Heading>
+            
+            <Input 
+              placeholder="Nome" 
+            />
+
+            <Input 
+              placeholder="E-mail"
+              keyboardType="email-address"
+              autoCapitalize="none"
+            />
+            <Input 
+              placeholder="Senha" 
+              secureTextEntry
+            />
+
+            <Button title="Criar e acessar"/>
+          </Center>
+
+          <Button title="Voltar para o login" variant="outline" mt="$12"/>
+        </VStack>
+
+      </VStack>
+    </ScrollView>
+  )
+}
