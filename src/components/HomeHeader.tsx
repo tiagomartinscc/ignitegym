af035ce4b09912feb59891eda638d5716f7a5471
Tiago Martins <tiagomartinscc@hotmail.com>
2025-03-13 14:23:52 -0300

    finish HomeHeader component

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,12 +1,22 @@
-import { Heading, HStack, Text, VStack } from "@gluestack-ui/themed";
+import { Heading, HStack, Icon, Text, VStack } from "@gluestack-ui/themed"
+import { UserPhoto } from "./UserPhoto"
+import { LogOut } from 'lucide-react-native'
 
 export function HomeHeader() {
   return (
-    <HStack bg="$gray600" pt="$16" pb="$5" px="$8" alignItems="center">
-      <VStack>
+    <HStack bg="$gray600" pt="$16" pb="$5" px="$8" alignItems="center" gap="$4">
+      <UserPhoto 
+        source={{uri: 'https://www.github.com/tiagomartinscc.png'}} 
+        w="$16"
+        h="$16"
+        alt='Imagem do usuário' 
+      />
+      <VStack flex={1}>
         <Text color="$gray100" fontSize="$sm">Olá, </Text>
         <Heading color="$gray100" fontSize="$md">Tiago Martins</Heading>
       </VStack>
+      
+      <Icon as={LogOut} color="$gray200" size='xl' />
     </HStack>
   )
 }
