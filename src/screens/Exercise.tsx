0766d1186defe938de4402b038c9495ef75c6fa3
Tiago Martins <tiagomartinscc@hotmail.com>
2025-03-14 15:04:26 -0300

    go back from exercise to home

--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -1,9 +1,25 @@
-import { Center, Text } from "@gluestack-ui/themed";
+import { TouchableOpacity } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+
+import { AppNavigatorRoutesProps } from '@routes/app.routes'
+
+import { Icon, VStack } from '@gluestack-ui/themed'
+import { ArrowLeft } from 'lucide-react-native'
 
 export function Exercise() {
+  const navigation = useNavigation<AppNavigatorRoutesProps>()
+
+  function handleGoBack() {
+    navigation.goBack()
+  }
+
   return (
-    <Center flex={1}>
-      <Text>Exercise</Text>
-    </Center>
+    <VStack flex={1}>
+      <VStack px="$8" bg="$gray600" pt="$12">
+        <TouchableOpacity onPress={handleGoBack}>
+          <Icon as={ArrowLeft} color="$green500" size="xl" />
+        </TouchableOpacity>
+      </VStack>
+    </VStack>
   )
 }
