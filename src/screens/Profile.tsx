86c222f936f09ad320308d59307a365253f0c6bf
Tiago Martins <tiagomartinscc@hotmail.com>
2025-03-14 14:14:06 -0300

    secrets inputs

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,8 +1,9 @@
 import { ScrollView, TouchableOpacity } from "react-native";
 import { ScreenHeader } from "@components/ScreenHeader";
-import { VStack, Text, Center } from "@gluestack-ui/themed";
+import { VStack, Text, Center, Heading } from "@gluestack-ui/themed";
 import { UserPhoto } from "@components/UserPhoto";
 import { Input } from "@components/Input";
+import { Button } from "@components/Button";
 
 export function Profile() {
   return (
@@ -30,6 +31,25 @@ export function Profile() {
         <Center w="$full" gap="$4">
           <Input placeholder="Nome" bg="$gray600" />
           <Input value="tiagomartinscc@hotmail.com" bg="$gray600" isReadOnly/>
+        </Center>
+
+        <Heading 
+          alignSelf="flex-start"
+          fontFamily="$heading"
+          color="$gray200"
+          fontSize="$md"
+          mt="$12"
+          mb="$2"
+        >
+          Alterar Senha
+        </Heading>
+
+        <Center w="$full" gap="$4">
+          <Input placeholder="Senha antiga" bg="$gray600" secureTextEntry />
+          <Input placeholder="Nova senha" bg="$gray600" secureTextEntry />
+          <Input placeholder="Confirme a nova senha" bg="$gray600" secureTextEntry />
+
+          <Button title="Atualizar" />
         </Center>
 
         </Center>
