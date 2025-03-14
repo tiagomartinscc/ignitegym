cda986938ebfaa74c2f630ca5843c54c3a1e2944
Tiago Martins <tiagomartinscc@hotmail.com>
2025-03-14 08:23:45 -0300

    user image into profile

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,10 +1,21 @@
+import { ScrollView } from "react-native";
 import { ScreenHeader } from "@components/ScreenHeader";
-import { VStack, Text } from "@gluestack-ui/themed";
+import { VStack, Text, Center } from "@gluestack-ui/themed";
+import { UserPhoto } from "@components/UserPhoto";
 
 export function Profile() {
   return (
     <VStack flex={1}>
       <ScreenHeader title="Perfil" />
+      <ScrollView contentContainerStyle={{ paddingBottom: 36}}>
+        <Center mt="$6" px="$10">
+          <UserPhoto 
+            source={{uri: 'https://github.com/tiagomartinscc.png'}} 
+            alt='Foto do usuário'
+            size="xl"
+          />
+        </Center>
+      </ScrollView>
     </VStack>
   )
 }
