cdcf773bd11d2fdf500aa2f21163b22f4ce218e5
Tiago Martins <tiagomartinscc@hotmail.com>
2025-03-14 15:09:08 -0300

    exercise image

--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -3,8 +3,17 @@ import { useNavigation } from '@react-navigation/native'
 
 import { AppNavigatorRoutesProps } from '@routes/app.routes'
 
-import { Icon, VStack } from '@gluestack-ui/themed'
+import {
+  Heading,
+  HStack,
+  Icon,
+  VStack,
+  Text,
+  Image,
+} from '@gluestack-ui/themed'
+
 import { ArrowLeft } from 'lucide-react-native'
+import BodySvg from '@assets/body.svg'
 
 export function Exercise() {
   const navigation = useNavigation<AppNavigatorRoutesProps>()
@@ -19,7 +28,43 @@ export function Exercise() {
         <TouchableOpacity onPress={handleGoBack}>
           <Icon as={ArrowLeft} color="$green500" size="xl" />
         </TouchableOpacity>
+
+        <HStack
+          justifyContent="space-between"
+          alignItems="center"
+          mt="$4"
+          mb="$8"
+        >
+          <Heading
+            color="$gray100"
+            fontFamily="$heading"
+            fontSize="$lg"
+            flexShrink={1}
+          >
+            Puxada frontal
+          </Heading>
+          <HStack alignItems="center">
+            <BodySvg />
+
+            <Text color="$gray200" ml="$1" textTransform="capitalize">
+              Costas
+            </Text>
+          </HStack>
+        </HStack>        
       </VStack>
+      <VStack p="$8">
+        <Image
+          source={{
+            uri: 'https://static.wixstatic.com/media/2edbed_60c206e178ad4eb3801f4f47fc6523df~mv2.webp/v1/fill/w_350,h_375,al_c/2edbed_60c206e178ad4eb3801f4f47fc6523df~mv2.webp',
+          }}
+          alt="Exercício"
+          mb="$3"
+          resizeMode="cover"
+          rounded="$lg"
+          w="$full"
+          h="$80"
+        />
+      </VStack>      
     </VStack>
   )
 }
