5a2e624531c951ed880eb6b1993e512bd4a60e78
Tiago Martins <tiagomartinscc@hotmail.com>
2025-03-15 21:28:51 -0300

    interface exercises

--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -1,4 +1,4 @@
-import { TouchableOpacity } from 'react-native'
+import { TouchableOpacity, ScrollView } from 'react-native'
 import { useNavigation } from '@react-navigation/native'
 
 import { AppNavigatorRoutesProps } from '@routes/app.routes'
@@ -10,10 +10,14 @@ import {
   VStack,
   Text,
   Image,
+  Box,
 } from '@gluestack-ui/themed'
 
 import { ArrowLeft } from 'lucide-react-native'
 import BodySvg from '@assets/body.svg'
+import SeriesSvg from '@assets/series.svg'
+import RepetitionsSvg from '@assets/repetitions.svg'
+import { Button } from '@components/Button'
 
 export function Exercise() {
   const navigation = useNavigation<AppNavigatorRoutesProps>()
@@ -23,7 +27,7 @@ export function Exercise() {
   }
 
   return (
-    <VStack flex={1}>
+<VStack flex={1}>
       <VStack px="$8" bg="$gray600" pt="$12">
         <TouchableOpacity onPress={handleGoBack}>
           <Icon as={ArrowLeft} color="$green500" size="xl" />
@@ -50,21 +54,52 @@ export function Exercise() {
               Costas
             </Text>
           </HStack>
-        </HStack>        
+        </HStack>
       </VStack>
-      <VStack p="$8">
-        <Image
-          source={{
-            uri: 'https://static.wixstatic.com/media/2edbed_60c206e178ad4eb3801f4f47fc6523df~mv2.webp/v1/fill/w_350,h_375,al_c/2edbed_60c206e178ad4eb3801f4f47fc6523df~mv2.webp',
-          }}
-          alt="Exercício"
-          mb="$3"
-          resizeMode="cover"
-          rounded="$lg"
-          w="$full"
-          h="$80"
-        />
-      </VStack>      
+
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 32 }}
+      >
+        <VStack p="$8">
+          <Image
+            source={{
+              uri: 'https://static.wixstatic.com/media/2edbed_60c206e178ad4eb3801f4f47fc6523df~mv2.webp/v1/fill/w_350,h_375,al_c/2edbed_60c206e178ad4eb3801f4f47fc6523df~mv2.webp',
+            }}
+            alt="Exercício"
+            mb="$3"
+            resizeMode="cover"
+            rounded="$lg"
+            w="$full"
+            h="$80"
+          />
+
+          <Box bg="$gray600" rounded="$md" pb="$4" px="$4">
+            <HStack
+              alignItems="center"
+              justifyContent="space-around"
+              mb="$6"
+              mt="$5"
+            >
+              <HStack>
+                <SeriesSvg />
+                <Text color="$gray200" ml="$2">
+                  3 séries
+                </Text>
+              </HStack>
+
+              <HStack>
+                <RepetitionsSvg />
+                <Text color="$gray200" ml="$2">
+                  12 repetições
+                </Text>
+              </HStack>
+            </HStack>
+
+            <Button title="Marcar como realizado" />
+          </Box>
+        </VStack>
+      </ScrollView>
     </VStack>
   )
 }
