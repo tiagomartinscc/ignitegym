48c94a2006c9721f7ed7bec530524e969000343b
Tiago Martins <tiagomartinscc@hotmail.com>
2025-03-17 10:44:18 -0300

    validation confirm password

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -22,7 +22,9 @@ const signUpSchema = yup.object({
   name: yup.string().required('Informe o nome.'),
   email: yup.string().required('Informe o e-mail.').email('E-mail inválido'),
   password: yup.string().required('Informe seua senha.').min(6, 'A senha deve ter pelo menos 6 caracteres.'),
-  password_confirm: yup.string().required('Informe a confirmação da senha.')
+  password_confirm: yup.string().required('Confirme a senha.').oneOf([
+    yup.ref("password"), ""
+  ], "A confirmação da senha não confere.")
 })
 
 export function SignUp() {
