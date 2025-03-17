4fb8747c6e7940a1a9977c87f0c71f9d082af9d6
Tiago Martins <tiagomartinscc@hotmail.com>
2025-03-17 09:55:53 -0300

    Field validation using rules into Controller

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -60,6 +60,9 @@ export function SignUp() {
             <Controller 
               control={control}
               name="name"
+              rules={{
+                required: 'Informe o seu nome'
+              }}
               render={({field: {onChange, value}}) => (
                 <Input 
                   placeholder="Nome"
