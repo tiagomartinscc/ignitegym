f11522c20d17fa4077c861aeb60a315163d873ca
Tiago Martins <tiagomartinscc@hotmail.com>
2025-03-18 09:36:56 -0300

    show defaul image user into header

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -3,13 +3,14 @@ import { UserPhoto } from "./UserPhoto"
 import { LogOut } from 'lucide-react-native'
 
 import { useAuth } from "@hooks/useAuth"
+import defaultUserPhotoImg from '@assets/userPhotoDefault.png'
 
 export function HomeHeader() {
   const {user} = useAuth()
   return (
     <HStack bg="$gray600" pt="$16" pb="$5" px="$8" alignItems="center" gap="$4">
       <UserPhoto 
-        source={{uri: 'https://www.github.com/tiagomartinscc.png'}} 
+        source={user.avatar? {uri: user.avatar} : defaultUserPhotoImg} 
         w="$16"
         h="$16"
         alt='Imagem do usuário' 
