a931568d875b2e02ae1f350ee1917a890292c231
Tiago Martins <tiagomartinscc@hotmail.com>
2025-03-18 10:17:17 -0300

    storing the token in async storage and setting it in requests

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,7 +1,10 @@
 import { createContext, ReactNode, useEffect, useState } from "react"
+
 import { UserDTO } from "@dtos/UserDTO"
 import { api } from "@services/api"
+
 import { storageUserGet, storageUserRemove, storageUserSave } from '@storage/storageUser'
+import { storageAuthTokenSave } from "@storage/storageAuthToken"
 
 export type AuthContextDataProps = {
   user: UserDTO
@@ -19,13 +22,29 @@ type AuthContextProviderProps = {
 export function AuthContextProvider({children}: AuthContextProviderProps) {
   const [user, setUser] = useState<UserDTO>({} as UserDTO)
   const [isLoadingUserStorageData, setIsLoadingUserStorageData] = useState(true)
+
+  async function storageUserAndToken(userData: UserDTO, token: string) {
+    try {
+      setIsLoadingUserStorageData(true)
+
+      api.defaults.headers.common['Authorization'] = `Bearer ${token}`
+
+      await storageUserSave(userData)
+      await storageAuthTokenSave(token)
+      setUser(userData)
+    } catch (error) {
+      throw error
+    } finally {
+      setIsLoadingUserStorageData(false)
+    }
+  }
+
   async function signIn(email: string, password:string) {
     try {
       const {data} = await api.post('/sessions', {email, password})
 
-      if(data.user) {
-        setUser(data.user)
-        storageUserSave(data.user)
+      if(data.user && data.token) {
+        await storageUserAndToken(data.user, data.token)
       }
     } catch(error) {
       throw error
--- src/storage/storageConfig.ts
+++ src/storage/storageConfig.ts
@@ -1,5 +1,7 @@
-const USER_STORAGE = '@gymignite:user'
+const USER_STORAGE = '@ignitegym:user'
+const AUTH_TOKEN_STORAGE = '@ignitegym:token'
 
 export {
-  USER_STORAGE
+  USER_STORAGE,
+  AUTH_TOKEN_STORAGE
 }
